9ba6145612c1ba4a4f4c8fce8db3c4452a6f73e9
mikkel agerlin <mikkel_ac@hotmail.com>
2024-09-03 20:52:18 +0200

    feat: indicator in party view

--- src/party/PartyMemberTile.tsx
+++ src/party/PartyMemberTile.tsx
@@ -9,6 +9,30 @@ import {
   shouldShowPowerPoints,
 } from "../character/character-logic/PowerPointLogic";
 import { StyledLink } from "../shared/StyledLink";
+import { TextElement } from "../shared/text/Text";
+import { FlexRow } from "../codex/shared/FlexRow";
+import { TemporaryEffect } from "../utils/types/TemporaryEffect";
+
+const EffectIndicator = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100%",
+  flex: 1,
+  padding: Theme.Spacing.small,
+  borderRadius: Theme.BorderRadius.small,
+  backgroundColor: Theme.Secondary[200],
+  fontSize: "0.8em",
+  overflow: "hidden",
+  p: {
+    height: "100%",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+    maxWidth: "100%",
+  },
+});
 
 type PartyMemberTileStyleProps = {
   $clickable: boolean;
@@ -36,10 +60,24 @@ type PartyMemberTileProps = {
   character: Character;
   isDM: boolean;
 };
+const compareEffects = (a: TemporaryEffect, b: TemporaryEffect) => {
+  if (a.duration === b.duration) {
+    return a.name.localeCompare(b.name);
+  }
+  return a.duration - b.duration;
+};
 const InnerTile = ({ character, isDM }: PartyMemberTileProps) => {
+  const tempEffects = Object.values(character.temporaryEffects ?? {}).toSorted(
+    compareEffects
+  );
+
+  const limitedEffects = tempEffects.slice(0, 3);
+  const leftOvers = tempEffects.length - limitedEffects.length;
   return (
     <PartyMemberTileStyle $clickable={isDM}>
-      <div>{getCharacterFullName(character)}</div>
+      <FlexRow>
+        <TextElement>{getCharacterFullName(character)}</TextElement>
+      </FlexRow>
       <ResourceCounter
         total={maxHealth}
         remaining={maxHealth - character.wounds}
@@ -60,6 +98,22 @@ const InnerTile = ({ character, isDM }: PartyMemberTileProps) => {
           color={Theme.PowerPoints}
         />
       ) : null}
+      {limitedEffects.length > 0 && (
+        <FlexRow $gap={Theme.Spacing.xsmall}>
+          {limitedEffects.map((effect) => (
+            <EffectIndicator key={effect.name + effect.duration}>
+              <TextElement title={effect.name}>
+                {effect.duration}: {effect.name}
+              </TextElement>
+            </EffectIndicator>
+          ))}
+          {leftOvers > 0 && (
+            <EffectIndicator>
+              <TextElement>+{leftOvers} more</TextElement>
+            </EffectIndicator>
+          )}
+        </FlexRow>
+      )}
     </PartyMemberTileStyle>
   );
 };
